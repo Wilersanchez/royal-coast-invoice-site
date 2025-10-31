daf06700e6e1b2de50cd1bae711d53a1a2694ccc
Wiler Sanchez <Wilersanchez@ufl.edu>
2025-10-31 12:49:24 -0400

    some fixes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,15 @@
 import { cn } from "@/lib/utils";
+import * as React from "react";
 
-interface ContainerProps extends React.ComponentProps<"div"> {
+// Use a type alias instead of an empty interface
+type ContainerProps = React.ComponentProps<"div">;
 
-}
-
-const Container = ( {children, className, ...props}: ContainerProps ) => {
-    return (
-        <div {...props} className={cn('max-w-5xl mx-auto px-5', className)}>
-            {children}
-        </div>
-    )
-}
+const Container = ({ children, className, ...props }: ContainerProps) => {
+  return (
+    <div {...props} className={cn("max-w-5xl mx-auto px-5", className)}>
+      {children}
+    </div>
+  );
+};
 
 export default Container;
